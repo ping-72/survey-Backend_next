825166a18cffaed4f853741afb5d2e05e1b3e308
Prince <kumarprince551178@gmai.com>
2024-12-02 00:57:39 +0530

    all files added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connectMongo from "@/lib/mongodb";
 
 export default function handler(req, res) {
+  connectMongo();
+  if (connectMongo) console.log("Connected to MongoDB");
   res.status(200).json({ name: "John Doe" });
 }
